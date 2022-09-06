3b7096b069d5de435c04088f872ad71e1a475147
GuilhermeJWT <guiromanno@gmail.com>
2022-09-06 15:19:15 -0300

    Add teste para converter Certificado Digital em Base64 String

--- src/test/java/br/com/systemsgs/DfeFiscalApplicationTests.java
+++ src/test/java/br/com/systemsgs/DfeFiscalApplicationTests.java
@@ -1,13 +1,20 @@
 package br.com.systemsgs;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Base64;
+
 class DfeFiscalApplicationTests {
 
-	@Test
-	void contextLoads() {
+	public static void main(String[] args) throws IOException {
+		System.out.println(fileToByte("C:/Users/Guilherme Santos/nfe/certificado_digital.pfx"));
+	}
+
+	private static String fileToByte(String caminhoArquivo) throws IOException{
+		byte[] fileContent = Files.readAllBytes(new File(caminhoArquivo).toPath());
+		return Base64.getEncoder().encodeToString(fileContent);
 	}
 
 }
