9d586fb7b82b4540200703273a9f10ff6b96bb68
GuilhermeJWT <guiromanno@gmail.com>
2022-09-06 11:03:05 -0300

    Add Repository Empresa e NotaEntrada

---
+++ src/main/java/br/com/systemsgs/repository/EmpresaRepository.java
@@ -0,0 +1,10 @@
+package br.com.systemsgs.repository;
+
+import br.com.systemsgs.model.Empresa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
+
+}
